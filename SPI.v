6064c0c4342ba53091cf4a77b512ece02894c246
Diego Santana <diegosantanaalba@gmail.com>
2026-06-25 12:35:24 -0600

    One Slave Module Finished

--- SPI.v
+++ SPI.v
@@ -24,16 +24,37 @@ always @(posedge clk ) begin
     end else begin
             case (state)
                 IDLE: begin
-                    
+                    if(enable==1) begin
+                        state <= START;
+                        busy <= 1;
+                    end else begin
+                        busy <= 0;
+                        cs <= 0;
+                    end
                 end 
                 START: begin
-                    
+                    cs <=1;
+                    shift_reg <= data;
+                    state <= TRANSFER;
                 end 
                 TRANSFER: begin
-                    
+                    sclk <= ~sclk;
+                    if (!sclk) begin
+                        mosi <= shift_reg[N-1];
+                        shift_reg <= {shift_reg[N-2:0], 1'b0};
+                    end else begin
+                        counter <= counter +1;
+                        if (counter == N-1) begin
+                            state <= DONE;
+                        end
+                    end
                 end
                 DONE: begin
-                    
+                    cs <= 0;
+                    busy <= 0;
+                    counter <= 0;
+                    sclk <= 0;
+                    state <= IDLE;
                 end
                 default: 
             endcase
